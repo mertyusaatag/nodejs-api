571a0889565ea20799144684696f3976cc0df095
mertyusaatag <mertatag0@gmail.com>
2021-08-28 16:43:14 +0300

    ssa

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 const express = require('express')
 const Flatted = require('flatted')
 const { passengerDatabase } = require('./database')
+const Passenger = require('./models/passenger')
 const app = express()
 const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// iki nokta üst üste req.params.çekilmek istenilen prop şeklinde
+app.get('/passenger/:passengerId' , async (req,res) => {
+  const passenger = await passengerDatabase.find(req.params.passengerId)
+  // if(!passenger)
+  // {
+  //   res.send("Böyle biri bulunamadı")
+  // }
+  if(!passenger) return res.status(404).send("Böyle biri bulunamadı")
+  res.send(Flatted.stringify(passenger))
+
+  // let passenger;
+  // try {
+  //   passenger = await passengerDatabase.find(req.params.passengerId)
+  // } catch (e) {
+  //   if (!passenger) return res.send('Cannot find passenger').sendStatus(404)
+  // }
+  // res.send(Flatted.stringify(passenger))
+  
+})
+
+app.post('/passengers', async(req,res)=> {
+  console.log(Flatted.stringify(req.body))
+  const person = Passenger.create(req.body)
+  
+  await passengerDatabase.insert(person)
+  res.send(person)
+})
+
 // farklı yazım
-async function onPassanger(req,res) {
-    const passangers = await passengerDatabase.load()
-    res.send(Flatted.stringify(passangers))
+async function onPassenger(req,res) {
+    const passengers = await passengerDatabase.load()
+    res.send(Flatted.stringify(passengers))
 }
 //2. parametreye gönderiyorum.
-app.get('/passangers',  onPassanger )
+app.get('/passengers',  onPassenger )
+
+
 
 app.listen(port, () => {
   console.log(`Bağlantı Başarılı... Port: ${port}`)
